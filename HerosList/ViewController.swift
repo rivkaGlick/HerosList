1ed4308db2c1d8787eba1a6718632a10cb753584
rivka <rivki311@gmail.com>
2021-05-02 23:15:30 +0300

    Initial Commit

---
+++ HerosList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HerosList
+//
+//  Created by rivki glick on 02/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
